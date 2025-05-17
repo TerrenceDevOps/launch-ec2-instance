6dba7b11915d91370c70a43116d30b107791ca4a
Terrence Ncube <terrencencube593@gmail.com>
2025-05-17 17:56:30 +0200

    committed auto apply

--- cicd/apply-terraform.sh
+++ cicd/apply-terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fail on any error
-set -eu
+#set -eu
 
 # go back to the previous directory
 cd .. 
@@ -13,4 +13,4 @@ terraform init
 terraform apply -auto-approve
 
 # destroy terraform
-# terraform destroy -auto-approve
+terraform destroy -auto-approve
